265119e58a9d6832d2551164c60aef8e29a191e0
Anna Golubeva <110536476+angolechka@users.noreply.github.com>
2023-04-04 17:18:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,6 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 82
+    answer = x.mean()/t
+    return answer # Ваш ответ
